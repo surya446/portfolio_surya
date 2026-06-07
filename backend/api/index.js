9902628b24e93eab1b87e82624e2e8582d9cbf73
surya <surya.u4759@gmail.com>
2026-06-07 17:05:07 +0530

    Update portfolio description

--- backend/api/index.js
+++ backend/api/index.js
@@ -16,6 +16,7 @@ async function dbConnect() {
   if (!cached.promise) {
     cached.promise = mongoose.connect(MONGO_URI, {
       useNewUrlParser: true,
+      
       useUnifiedTopology: true,
     }).then((mongoose) => {
       console.log("✅ MongoDB connected");
